15b97eeae0e2bdcd7cbd3cfb7fed23a4c4a93a2f
adipothula <adipothula@hotmail.com>
2025-04-21 22:45:07 -0400

    added addtional crud operations

--- src/main/java/com/example/RestDemo/controller/CloudVendorAPIService.java
+++ src/main/java/com/example/RestDemo/controller/CloudVendorAPIService.java
@@ -1,6 +1,10 @@
 package com.example.RestDemo.controller;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,5 +21,24 @@ public class CloudVendorAPIService {
         return oCloudVendor;
         /* new CloudVendor("C1","Vendor 1","Address One","xxxxx"); */
     }
+
+    @PostMapping
+    public String creatCloudVendor(@RequestBody CloudVendor oCloudVendor){
+        this.oCloudVendor = oCloudVendor;
+        return "Cloud Vendor Created Successfully";
+    }
+
+    @PutMapping
+    public String updateCloudVendor(@RequestBody CloudVendor oCloudVendor){
+        this.oCloudVendor = oCloudVendor;
+        return "Cloud Vendor Updated Successfully"; 
+    }
+
+    @DeleteMapping("{vendorId}")
+    public String deleteCloudVendor(String vendorId){
+        this.oCloudVendor = null;
+        return "Cloud Vendor Deleted Successfully";
+    }
+
 }
 
